5500e5c17331ad38032041d7cb002c00bba7e752
Nikita Bivol <n.bivol2000@yandex.ru>
2024-02-21 20:53:14 +0300

    Add name

--- src/main/kotlin/lesson_1.kt
+++ src/main/kotlin/lesson_1.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>) {
 //    println("Hello World!")
 
     val numberOfOrders: Int = 42
-    val mesege = "не забудь важное"
+    val mesege = "не забудь важное, Никита"
 
     println(numberOfOrders)
     println(mesege)

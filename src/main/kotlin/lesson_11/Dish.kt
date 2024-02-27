082ed056d70e1d7578558624bc06f053507790ff
Nikita Bivol <n.bivol2000@yandex.ru>
2024-02-27 19:09:28 +0300

    add package lesson_11

---
+++ src/main/kotlin/lesson_11/Dish.kt
@@ -0,0 +1,31 @@
+package lesson_11
+
+class Dish(
+    val id: Int,
+    val name: String,
+    var category: String,
+    val ingredients: List<String>,
+    var inFavorites: Boolean = false,
+) {
+
+    fun addToFavorites() {
+        println("Блюдо $name дбавлено в избранное")
+        inFavorites = true
+    }
+
+    fun removeFromFavorites() {
+        println("Блюдо $name удалено из избранного")
+        inFavorites = false
+
+    }
+
+    fun startCooking() {
+        println("Пользователь перешел на экран приготовления блюда $name")
+    }
+
+    fun downloadIngredients(): List<String> {
+        return ingredients
+
+    }
+
+}
